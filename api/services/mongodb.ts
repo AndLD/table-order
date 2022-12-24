83d66cc1ea5c7c6c1eab592fca5ef131c67cc5b6
AndLD <nil10035@gmail.com>
2022-12-24 21:09:30 +0200

    TO-1

--- api/services/mongodb.ts
+++ api/services/mongodb.ts
@@ -2,13 +2,21 @@ import { getLogger } from 'log4js'
 import mongoose from 'mongoose'
 
 const logger = getLogger('services/mongodb')
+const MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING
 
 async function init() {
-    await mongoose
-        .connect('mongodb+srv://root:Fatal544@cluster1.zhful.mongodb.net/?retryWrites=true&w=majority')
-        .then(() => {
-            logger.info('MongoDB successfully connected.')
-        })
+    if (MONGO_DB_CONNECTION_STRING) {
+        mongoose
+            .connect(MONGO_DB_CONNECTION_STRING)
+            .then(() => {
+                logger.info('MongoDB successfully connected.')
+            })
+            .catch((err) => {
+                logger.error(err)
+            })
+    } else {
+        logger.error('Connecting error: MONGO_DB_CONNECTION_STRING missed')
+    }
 }
 
 export const mongodbService = {

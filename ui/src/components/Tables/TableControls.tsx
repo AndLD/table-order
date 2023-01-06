56c38f5c041e294d8999b6d9c4384eeb11656211
ManyaDev <marysy.manyaa@gmail.com>
2023-01-06 12:25:44 +0200

    TO-12

--- ui/src/components/Tables/TableControls.tsx
+++ ui/src/components/Tables/TableControls.tsx
@@ -1,4 +1,5 @@
-import { Button, Popconfirm } from 'antd'
+import { Button, DatePicker, Popconfirm } from 'antd'
+import moment from 'moment'
 import { useContext } from 'react'
 import { tablesContext } from '../../contexts'
 import { useDeleteTable } from '../../hooks/graphql/mutations/tables'
@@ -13,8 +14,24 @@ export default function TableControls() {
 
     const deleteTable = useDeleteTable()
 
+    const onOk = (value: any) => {
+        console.log('onOk: ', moment().format())
+        console.log('onOk: ', typeof value)
+        console.log('onOk: ', value.format('YYYY-MM-DD HH:mm:ss'))
+    }
+
     return (
         <div style={{ textAlign: 'right', margin: '0 0 16px 0' }}>
+            <DatePicker
+                format="YYYY-MM-DD HH:mm:ss"
+                disabledDate={(current) => {
+                    let customDate = moment().format('YYYY-MM-DD')
+                    return current && current < moment(customDate, 'YYYY-MM-DD')
+                }}
+                disabledTime={(current) => current?.isBefore(moment()) as any}
+                showTime
+                onOk={onOk}
+            />
             <Button
                 style={{ margin: '0 0 0 5px' }}
                 type="primary"
